76c025d4b420673df469c6f8742ab0c2f6037c2c
Karthiga <karthiga@bitzbillo.com>
2020-06-12 11:04:36 +0530

    Mi1-mainnet files

---
+++ src/lib_p2p/p2p_io_scheduler.mli
@@ -0,0 +1,120 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Generic IO scheduling between file descriptors.
+
+    In order to use IO scheduling, the [register] function must be
+    used to make a file descriptor managed by a [scheduler].. It will
+    return a value of type [connection] that must be used to perform IO
+    on the managed file descriptor using this module's dedicated IO
+    functions (read, write, etc.).
+
+    Each connection is allowed a read (resp. write) quota, which is
+    for now fairly distributed among connections.
+
+    To each connection is associated a read (resp. write) queue where
+    data is copied to (resp. read from), at a rate of
+    max_download_speed / num_connections (resp. max_upload_speed /
+    num_connections).
+*)
+
+(** Type of a connection. *)
+type connection
+
+(** Type of an IO scheduler. *)
+type t
+
+(** [create ~max_upload_speed ~max_download_speed ~read_queue_size
+    ~write_queue_size ()] is an IO scheduler with specified (global)
+    max upload (resp. download) speed, and specified read
+    (resp. write) queue sizes (in bytes) for connections. *)
+val create :
+  ?max_upload_speed:int ->
+  ?max_download_speed:int ->
+  ?read_queue_size:int ->
+  ?write_queue_size:int ->
+  read_buffer_size:int ->
+  unit ->
+  t
+
+(** [register sched fd] is a [connection] managed by [sched]. *)
+val register : t -> P2p_fd.t -> connection
+
+(** [write conn msg] returns [Ok ()] when [msg] has been added to
+    [conn]'s write queue, or fail with an error. *)
+val write :
+  ?canceler:Lwt_canceler.t -> connection -> Bytes.t -> unit tzresult Lwt.t
+
+(** [write_now conn msg] is [true] iff [msg] has been (immediately)
+    added to [conn]'s write queue, [false] if it has been dropped. *)
+val write_now : connection -> Bytes.t -> bool
+
+(** [read_now conn ~pos ~len buf] blits at most [len] bytes from
+    [conn]'s read queue and returns the number of bytes written in
+    [buf] starting at [pos]. *)
+val read_now :
+  connection -> ?pos:int -> ?len:int -> Bytes.t -> int tzresult option
+
+(** Like [read_now], but waits till [conn] read queue has at least one
+    element instead of failing. *)
+val read :
+  ?canceler:Lwt_canceler.t ->
+  connection ->
+  ?pos:int ->
+  ?len:int ->
+  Bytes.t ->
+  int tzresult Lwt.t
+
+(** Like [read], but blits exactly [len] bytes in [buf]. *)
+val read_full :
+  ?canceler:Lwt_canceler.t ->
+  connection ->
+  ?pos:int ->
+  ?len:int ->
+  Bytes.t ->
+  unit tzresult Lwt.t
+
+(** [stat conn] is a snapshot of current bandwidth usage for
+    [conn]. *)
+val stat : connection -> P2p_stat.t
+
+(** [global_stat sched] is a snapshot of [sched]'s bandwidth usage
+    (sum of [stat conn] for each [conn] in [sched]). *)
+val global_stat : t -> P2p_stat.t
+
+(** [iter_connection sched f] applies [f] on each connection managed
+    by [sched]. *)
+val iter_connection : t -> (connection -> unit) -> unit
+
+(** [close conn] cancels [conn] and returns after any pending data has
+    been sent. *)
+val close : ?timeout:float -> connection -> unit tzresult Lwt.t
+
+(** [shutdown sched] returns after all connections managed by [sched]
+    have been closed and [sched]'s inner worker has successfully
+    canceled. *)
+val shutdown : ?timeout:float -> t -> unit Lwt.t
+
+val id : connection -> int
---
+++ tests_python/tests/test_contract.py
@@ -0,0 +1,321 @@
+import os
+import subprocess
+import pytest
+from tools import utils, paths, constants
+
+CONTRACT_PATH = f'{paths.TEZOS_HOME}/src/bin_client/test/contracts'
+
+BAKE_ARGS = ['--minimal-timestamp']
+
+
+def file_basename(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def originate(client,
+              session,
+              contract,
+              init_storage,
+              amount,
+              contract_name=None,
+              sender='bootstrap1',
+              baker='bootstrap5'):
+    if contract_name is None:
+        contract_name = file_basename(contract)
+    args = ['--init', init_storage, '--burn-cap', '10.0']
+    origination = client.originate(contract_name, amount,
+                                   sender, contract, args)
+    session['contract'] = origination.contract
+    print(origination.contract)
+    client.bake(baker, BAKE_ARGS)
+    assert utils.check_block_contains_operations(client,
+                                                 [origination.operation_hash])
+
+
+@pytest.mark.contract
+class TestManager:
+
+    def test_manager_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/entrypoints/manager.tz'
+        pubkey = constants.IDENTITIES['bootstrap2']['identity']
+        originate(client, session, path, f'"{pubkey}"', 1000)
+        originate(client, session, path, f'"{pubkey}"', 1000, contract_name="manager2")
+
+    def test_delegatable_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/entrypoints/delegatable_target.tz'
+        pubkey = constants.IDENTITIES['bootstrap2']['identity']
+        originate(client, session, path,
+                  f'Pair "{pubkey}" (Pair "hello" 45)', 1000)
+
+    def test_target_with_entrypoints_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/entrypoints/big_map_entrypoints.tz'
+        originate(client, session, path, 'Pair {} {}', 1000,
+                  contract_name='target')
+
+    def test_target_without_entrypoints_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/entrypoints/no_entrypoint_target.tz'
+        originate(client, session, path, 'Pair "hello" 42', 1000,
+                  contract_name='target_no_entrypoints')
+
+    def test_target_without_default_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/entrypoints/no_default_target.tz'
+        originate(client, session, path, 'Pair "hello" 42', 1000,
+                  contract_name='target_no_default')
+
+    def test_target_with_root_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/entrypoints/rooted_target.tz'
+        originate(client, session, path, 'Pair "hello" 42', 1000,
+                  contract_name='rooted_target')
+
+    def test_manager_set_delegate(self, client):
+        client.set_delegate('manager', 'bootstrap2', [])
+        client.bake('bootstrap5', BAKE_ARGS)
+        client.set_delegate('delegatable_target', 'bootstrap2', [])
+        client.bake('bootstrap5', BAKE_ARGS)
+        delegate = constants.IDENTITIES['bootstrap2']['identity']
+        assert client.get_delegate('manager', []) == delegate
+        assert client.get_delegate('delegatable_target', []) == delegate
+        client.set_delegate('manager', 'bootstrap3', [])
+        client.bake('bootstrap5', BAKE_ARGS)
+        client.set_delegate('delegatable_target', 'bootstrap3', [])
+        client.bake('bootstrap5', BAKE_ARGS)
+        delegate = constants.IDENTITIES['bootstrap3']['identity']
+        assert client.get_delegate('manager', []) == delegate
+        assert client.get_delegate('delegatable_target', []) == delegate
+
+    def test_manager_withdraw_delegate(self, client):
+        client.withdraw_delegate('manager', [])
+        client.bake('bootstrap5', BAKE_ARGS)
+        client.withdraw_delegate('delegatable_target', [])
+        client.bake('bootstrap5', BAKE_ARGS)
+        assert client.get_delegate('manager', []) == "none"
+        assert client.get_delegate('delegatable_target', []) == "none"
+
+    def test_transfer_to_manager(self, client):
+        balance = client.get_mutez_balance('manager')
+        balance_bootstrap = client.get_mutez_balance('bootstrap2')
+        amount = 10.001
+        amount_mutez = utils.mutez_of_tez(amount)
+        client.transfer(amount, 'bootstrap2', 'manager', ['--gas-limit', '15285'])
+        client.bake('bootstrap5', BAKE_ARGS)
+        new_balance = client.get_mutez_balance('manager')
+        new_balance_bootstrap = client.get_mutez_balance('bootstrap2')
+        fee = 0.00178
+        fee_mutez = utils.mutez_of_tez(fee)
+        assert balance + amount_mutez == new_balance
+        assert (balance_bootstrap - fee_mutez - amount_mutez
+                == new_balance_bootstrap)
+
+    def test_simple_transfer_from_manager_to_implicit(self, client):
+        balance = client.get_mutez_balance('manager')
+        balance_bootstrap = client.get_mutez_balance('bootstrap2')
+        amount = 10.1
+        amount_mutez = utils.mutez_of_tez(amount)
+        client.transfer(amount, 'manager', 'bootstrap2',
+                        ['--gas-limit', '26183'])
+        client.bake('bootstrap5', BAKE_ARGS)
+        new_balance = client.get_mutez_balance('manager')
+        new_balance_bootstrap = client.get_mutez_balance('bootstrap2')
+        fee = 0.002931
+        fee_mutez = utils.mutez_of_tez(fee)
+        assert balance - amount_mutez == new_balance
+        assert (balance_bootstrap + amount_mutez - fee_mutez
+                == new_balance_bootstrap)
+
+    def test_transfer_from_manager_to_manager(self, client):
+        balance = client.get_mutez_balance('manager')
+        balance_dest = client.get_mutez_balance('manager2')
+        balance_bootstrap = client.get_mutez_balance('bootstrap2')
+        amount = 10
+        amount_mutez = utils.mutez_of_tez(amount)
+        client.transfer(amount, 'manager', 'manager2',
+                        ['--gas-limit', '44625'])
+        client.bake('bootstrap5', BAKE_ARGS)
+        new_balance = client.get_mutez_balance('manager')
+        new_balance_dest = client.get_mutez_balance('manager2')
+        new_balance_bootstrap = client.get_mutez_balance('bootstrap2')
+        fee = 0.004804
+        fee_mutez = utils.mutez_of_tez(fee)
+        assert balance_bootstrap - fee_mutez == new_balance_bootstrap
+        assert balance - amount_mutez == new_balance
+        assert balance_dest + amount_mutez == new_balance_dest
+
+    def test_transfer_from_manager_to_default(self, client):
+        client.transfer(10, 'manager', 'bootstrap2',
+                        ['--entrypoint', 'default'])
+        client.bake('bootstrap5', BAKE_ARGS)
+        client.transfer(10, 'manager', 'manager',
+                        ['--entrypoint', 'default'])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_transfer_from_manager_to_target(self, client):
+        client.transfer(10, 'manager', 'target', ['--burn-cap', '0.356'])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_transfer_from_manager_to_entrypoint_with_args(self, client):
+        arg = 'Pair "hello" 42'
+        client.transfer(0, 'manager', 'target',
+                        ['--entrypoint', 'add_left',
+                         '--arg', arg,
+                         '--burn-cap', '0.068'
+                        ])
+        client.bake('bootstrap5', BAKE_ARGS)
+        client.transfer(0, 'manager', 'target',
+                        ['--entrypoint', 'mem_left',
+                         '--arg', '"hello"'])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_transfer_from_manager_no_entrypoint_with_args(self, client):
+        arg = 'Left Unit'
+        client.transfer(0, 'manager', 'target_no_entrypoints',
+                        ['--arg', arg])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_transfer_from_manager_to_no_default_with_args(self, client):
+        arg = 'Left Unit'
+        client.transfer(0, 'manager', 'target_no_default',
+                        ['--arg', arg])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_transfer_from_manager_to_rooted_target_with_args(self, client):
+        arg = 'Left Unit'
+        client.transfer(0, 'manager', 'rooted_target',
+                        ['--arg', arg,
+                         '--entrypoint', 'root'])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+
+def all_contracts():
+    directories = ['attic', 'opcodes']
+    contracts = []
+    for directory in directories:
+        for contract in os.listdir(f'{CONTRACT_PATH}/{directory}'):
+            contracts.append(f'{directory}/{contract}')
+    return contracts
+
+
+@pytest.mark.slow
+@pytest.mark.contract
+class TestContracts:
+    """Test type checking and execution of a bunch of contracts"""
+
+    def test_gen_keys(self, client):
+        client.gen_key('foo')
+        client.gen_key('bar')
+
+    @pytest.mark.parametrize("contract", all_contracts())
+    def test_typecheck(self, client, contract):
+        if contract.endswith('.tz'):
+            client.typecheck(f'{CONTRACT_PATH}/{contract}')
+
+    # TODO add more tests here
+    @pytest.mark.parametrize("contract,param,storage,expected",
+                             [('opcodes/ret_int.tz', 'None', 'Unit',
+                               '(Some 300)')])
+    def test_run(self, client, contract, param, storage, expected):
+        if contract.endswith('.tz'):
+            contract = f'{CONTRACT_PATH}/{contract}'
+            run_script_res = client.run_script(contract, param, storage)
+            assert run_script_res.storage == expected
+
+
+FIRST_EXPLOSION = '''
+{ parameter unit;
+  storage unit;
+  code{ DROP; PUSH nat 0 ;
+        DUP ; PAIR ;
+        DUP ; PAIR ;
+        DUP ; PAIR ;
+        DUP ; PAIR ;
+        DUP ; PAIR ;
+        DUP ; PAIR ;
+        DUP ; PAIR ;
+        DUP ; PAIR } }
+'''
+
+
+SECOND_EXPLOSION = '''
+{ parameter (list int) ;
+  storage (list (list (list int))) ;
+  code { CAR ; DIP { NIL (list int) } ;
+         DUP ; ITER { DROP ; DUP ; DIP { CONS } } ;
+         DROP ; DIP { NIL (list (list int)) } ;
+         DUP ; ITER { DROP ; DUP ; DIP { CONS } } ;
+         DROP ; NIL operation ; PAIR } }
+'''
+
+
+@pytest.mark.contract
+class TestGasBound:
+
+    def test_write_contract(self, tmpdir, session):
+        items = {'first_explosion.tz': FIRST_EXPLOSION,
+                 'second_explosion.tz': SECOND_EXPLOSION}.items()
+        for name, script in items:
+            contract = f'{tmpdir}/{name}'
+            with open(contract, 'w') as contract_file:
+                contract_file.write(script)
+                session[name] = contract
+
+    def test_originate_first_explosion(self, client, session):
+        name = 'first_explosion.tz'
+        contract = session[name]
+        # TODO client.typecheck(contract) -> type error not what we expect?
+        args = ['-G', '8000', '--burn-cap', '10']
+        with pytest.raises(subprocess.CalledProcessError) as _exc:
+            client.originate(f'{name}', 0, 'bootstrap1', contract, args)
+        # TODO capture output and check error message is correct
+
+    def test_originate_second_explosion(self, client, session):
+        name = 'second_explosion.tz'
+        contract = session[name]
+        storage = '{}'
+        inp = '{1;2;3;4;5;6;7;8;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1}'
+        client.run_script(contract, storage, inp)
+
+    # TODO complete with tests from test_contract.sh
+
+
+@pytest.mark.contract
+class TestChainId:
+
+    def test_chain_id_opcode(self, client, session):
+        path = f'{CONTRACT_PATH}/opcodes/chain_id.tz'
+        originate(client, session, path, 'Unit', 0)
+        client.transfer(0, 'bootstrap2', "chain_id", [])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_chain_id_authentication_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/mini_scenarios/authentication.tz'
+        pubkey = constants.IDENTITIES['bootstrap1']['public']
+        originate(client, session, path, f'Pair 0 "{pubkey}"', 1000)
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_chain_id_authentication_first_run(self, client, session):
+        destination = constants.IDENTITIES['bootstrap2']['identity']
+        operation = '{DROP; NIL operation; ' + \
+            f'PUSH address "{destination}"; ' + \
+            'CONTRACT unit; ASSERT_SOME; PUSH mutez 1000; UNIT; ' + \
+            'TRANSFER_TOKENS; CONS}'
+        chain_id = client.rpc('get', 'chains/main/chain_id')
+        contract_address = session['contract']
+        packed = client.pack(
+            f'Pair (Pair "{chain_id}" "{contract_address}") ' +
+            f'(Pair {operation} 0)',
+            'pair (pair chain_id address)' +
+            '(pair (lambda unit (list operation)) nat)')
+        signature = client.sign(packed, "bootstrap1")
+        client.transfer(0, 'bootstrap2', 'authentication',
+                        ['--arg', f'Pair {operation} \"{signature}\"'])
+        client.bake('bootstrap5', BAKE_ARGS)
+
+@pytest.mark.contract
+class TestBigMapToSelf:
+
+    def test_big_map_to_self_origination(self, client, session):
+        path = f'{CONTRACT_PATH}/opcodes/big_map_to_self.tz'
+        originate(client, session, path, '{}', 0)
+        client.bake('bootstrap5', BAKE_ARGS)
+
+    def test_big_map_to_self_transfer(self, client):
+        client.transfer(0, 'bootstrap2', "big_map_to_self", [])
